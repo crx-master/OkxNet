9b5130598de70d8bdae51175ebb649293478e4bf
CRX Master <crx-master@crucial.com.br>
2022-09-14 09:46:49 -0300

    Change OKX timestamp from DateTime to DateTimeOffset

--- OkxNet/Converters/OkexTimestampConverter.cs
+++ OkxNet/Converters/OkexTimestampConverter.cs
@@ -11,21 +11,28 @@ namespace OkxNet.Converters
         /// <inheritdoc />
         public override bool CanConvert(Type objectType)
         {
-            return objectType == typeof(DateTime);
+            return objectType == typeof(DateTimeOffset);
         }
 
         /// <inheritdoc />
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            if (reader.Value == null)
-                return null;
+            try
+            {
+                if (reader.Value == null)
+                    return null;
 
-            if (reader.Value is string s && string.IsNullOrWhiteSpace(s))
-                return null;
+                if (reader.Value is string s && string.IsNullOrWhiteSpace(s))
+                    return null;
 
-            var t = long.Parse(reader.Value.ToString());
-            return DateTimeOffset.FromUnixTimeMilliseconds(t);
-            //return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(t);
+                var t = long.Parse(reader.Value.ToString());
+                return DateTimeOffset.FromUnixTimeMilliseconds(t);
+                //return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(t);
+            }
+            catch(Exception ex)
+            {
+                throw new ApplicationException($"Cannot convert value '{reader.Value}' to type {nameof(DateTimeOffset)}.", ex);
+            }
         }
 
         /// <inheritdoc />
